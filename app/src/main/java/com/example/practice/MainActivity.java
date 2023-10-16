07cbe8ece22b7bfc028cc86acecbf5ae8b55a5d2
krish <krishdobariya117@gmail.com>
2023-10-16 14:33:38 +0530

    First

---
+++ app/src/main/java/com/example/practice/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.practice;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import com.example.practice.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        binding.seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
